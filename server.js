49106e4578b0fbdb33879808d0f5f1c8498b8190
LambeAwwal <lambeawwal7@gmail.com>
2024-04-05 23:02:03 +0100

    database updated

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const port = 3000;
 
 // Create a new pool instance
 const client = new Client({
-    user: 'endowed_user',
-    host: 'dpg-co78i9f109ks73845nlg-a',
-    database: 'endowed',
-    password: 'vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM',
+    user: "endowed_user",
+    host: "dpg-co78i9f109ks73845nlg-a",
+    database: "endowed",
+    password: "vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM",
     port: 5432, // Default PostgreSQL port
 //  Internal Database URL: "postgres://endowed_user:vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM@dpg-co78i9f109ks73845nlg-a/endowed",
 //  External Database URL: "postgres://endowed_user:vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM@dpg-co78i9f109ks73845nlg-a.oregon-postgres.render.com/endowed",
@@ -42,9 +42,8 @@ app.post('/register', async (req, res) => {
         await client.query('INSERT INTO users (first_name, last_name, other_names, mobile_number, email_address, level, social_media, recommendation) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
             [userData.firstName, userData.lastName, userData.otherNames, userData.mobileNumber, userData.emailAddress, userData.level, userData.Socialmedia, userData.recommendation]);
 
-        // If insertion is successful, send a response indicating successful registration
-        const alertScript = '<script>alert("Registration successful!"); window.location.href = "/";</script>';
-        res.send(alertScript);
+        // If insertion is successful, redirect to registration success page
+        res.redirect('/successful.html');
     } catch (error) {
         // Log and handle any errors that occur during insertion
         console.error('Error registering user:', error);
