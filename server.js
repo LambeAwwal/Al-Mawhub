c4c609fc7b6cce6c415660cb47a28e1f7be7435d
LambeAwwal <lambeawwal7@gmail.com>
2024-04-05 15:27:04 +0100

    database updated

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const client = new Client({
     host: 'dpg-co78i9f109ks73845nlg-a',
     database: 'endowed',
     password: 'vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM',
-    Internal Database URL: 'postgres://endowed_user:vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM@dpg-co78i9f109ks73845nlg-a/endowed',
-    External Database URL: 'postgres://endowed_user:vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM@dpg-co78i9f109ks73845nlg-a.oregon-postgres.render.com/endowed',
-    PSQL Command: 'PGPASSWORD=vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM psql -h dpg-co78i9f109ks73845nlg-a.oregon-postgres.render.com -U endowed_user endowed',
     port: 5432, // Default PostgreSQL port
+ 
+ External Database URL: "postgres://endowed_user:vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM@dpg-co78i9f109ks73845nlg-a.oregon-postgres.render.com/endowed",
+ PSQL Command: "PGPASSWORD=vv0LBLAQ2j1WGfjaPVDZunhDknhST3oM psql -h dpg-co78i9f109ks73845nlg-a.oregon-postgres.render.com -U endowed_user endowed",
 });
 client.connect();
 
