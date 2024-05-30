5212a9f97823067a49efa2f0315ab050eba6c259
LambeAwwal <lambeawwal7@gmail.com>
2024-05-30 23:24:51 +0100

    updated

--- server.js
+++ server.js
@@ -6,7 +6,22 @@ const app = express();
 const port = 3000;
 const PORT = process.env.PORT || 3000;
 // Create a new pool instance
-
+const pool = new Pool({
+    user: "postgres",
+    host: "localHost",
+    database: "Endowed",
+    password: "12345",
+    port: 5432,
+    // user: "the_endowed_user",
+    // host: "dpg-cokgutv79t8c73cafu80-a",
+    // database: "the_endowed",
+    // password: "CLPP3rOkQmcXuv2UGod82cemYlvqL6Qp",
+    // port: 5432, // Default PostgreSQL port
+//  Internal Database URL: "postgres://the_endowed_user:CLPP3rOkQmcXuv2UGod82cemYlvqL6Qp@dpg-cokgutv79t8c73cafu80-a/the_endowed",
+//  External Database URL: "postgres://the_endowed_user:CLPP3rOkQmcXuv2UGod82cemYlvqL6Qp@dpg-cokgutv79t8c73cafu80-a.oregon-postgres.render.com/the_endowed",
+//  PSQL Command: "PGPASSWORD=CLPP3rOkQmcXuv2UGod82cemYlvqL6Qp psql -h dpg-cokgutv79t8c73cafu80-a.oregon-postgres.render.com -U the_endowed_user the_endowed",
+});
+// connectionString = `postgres://${firstName}: ${Password}@${Host}:${Port}/${DataBase}`;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -27,7 +42,19 @@ app.post('/register', async (req, res) => {
         recommendation: req.body.recommendation
     };
 
-  
+  try {
+        // Insert user data into the database
+        const client = await pool.connect();
+        await client.query('INSERT INTO users (first_name, last_name, other_names, mobile_number, email_address, level, social_media, recommendation) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
+            [userData.firstName, userData.lastName, userData.otherNames, userData.mobileNumber, userData.emailAddress, userData.level, userData.socialMedia, userData.recommendation])
+            await client.release();
+        // If insertion is successful, redirect to registration success page
+        res.redirect('/successful.html');
+    } catch (error) {
+        // Log and handle any errors that occur during insertion
+        console.error('Error registering user:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
     async function registerUser() {
